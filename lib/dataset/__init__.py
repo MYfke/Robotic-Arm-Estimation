cf0521fc938a490e84c7aae96de8aceb6e769a90
MYfke <myfke021@gmail.com>
2022-11-04 22:21:45 +0800

    完善__init__

--- lib/dataset/__init__.py
+++ lib/dataset/__init__.py
@@ -1,3 +1,7 @@
 
 from .mpii import MPIIDataset as mpii
 from .multiview_mpii import MultiviewMPIIDataset as multiview_mpii
+
+from .RoboArm import RoboArmDataset as RoboArm
+from .multiview_RoboArm import MultiviewRoboArmDataset as multiview_RoboArm
+
