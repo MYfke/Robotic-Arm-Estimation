5954587f2bdc77f8881e0a4a6447bec5f16949ef
MYfke <myfke021@gmail.com>
2022-11-11 18:07:36 +0800

    删除mpii数据集

--- lib/dataset/__init__.py
+++ lib/dataset/__init__.py
@@ -1,7 +1,3 @@
-
-from .mpii import MPIIDataset as mpii
-from .multiview_mpii import MultiviewMPIIDataset as multiview_mpii
-
-from .RoboArm import RoboArmDataset as RoboArm
+from .RoboArm_train import RoboArmDataset as RoboArm
 from .multiview_RoboArm import MultiviewRoboArmDataset as multiview_RoboArm
 
